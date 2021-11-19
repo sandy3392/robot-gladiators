577dc15240913d36c95e470a72c552de090cdff8
Sandeep Vittala <applesandy33@gmail.com>
2021-11-18 20:03:20 -0500

    logic for enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,16 +2,12 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-    // Alert players that they are starting the round
+var fight = function(enemyName) {
+
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
    
@@ -64,4 +60,7 @@ var fight = function() {
     }
   };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+  console.log(i);
+}
